2350905539aea2d859fc5499402b2962f77ba224
Theodoros <83337690+theodorosP@users.noreply.github.com>
2022-04-14 21:38:31 -0400

    Update test_theano.py

--- test_theano.py
+++ test_theano.py
@@ -1,8 +1,18 @@
 import theano
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 a = theano.tensor.dscalar()
 b = theano.tensor.dscalar()
-
 s = a + b
-f = theano.function([a, b], s)
-print(f(1,4))
+funct = theano.function([a, b], s)
+g = funct(1, 3)
+print(g)
+
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+add = tf.add(a, b)
+sess = tf.Session()
+binding = {a: 1.5, b: 2.5}
+c = sess.run(add, feed_dict=binding)
+print(c)
