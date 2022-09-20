febf4455ee41d39375c6277a8c99be62bf437798
Theodoros <83337690+theodorosP@users.noreply.github.com>
2022-09-19 23:43:25 -0400

    Update save_model_json.py

--- useful_functions/save_models/save_model_json.py
+++ useful_functions/save_models/save_model_json.py
@@ -1,7 +1,7 @@
 def save_json_model(m):
   import keras.models as keras_models
-  model_json = model.to_json()
+  model_json = m.to_json()
   with open("model.json", "w") as json_file:
     json_file.write(model_json)
-  w = model.save_weights("model.h5")
+  w = m.save_weights("model.h5")
   return json_file, w
